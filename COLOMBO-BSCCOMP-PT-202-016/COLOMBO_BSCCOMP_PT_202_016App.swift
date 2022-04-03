906d8fd1dd0e838839995ed5c0238cbe168aaa23
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-03 08:12:10 -0700

    Initial Commit

---
+++ COLOMBO-BSCCOMP-PT-202-016/COLOMBO_BSCCOMP_PT_202_016App.swift
@@ -0,0 +1,17 @@
+//
+//  COLOMBO_BSCCOMP_PT_202_016App.swift
+//  COLOMBO-BSCCOMP-PT-202-016
+//
+//  Created by Sasri on 2022-04-03.
+//
+
+import SwiftUI
+
+@main
+struct COLOMBO_BSCCOMP_PT_202_016App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COLOMBO-BSCCOMP-PT-202-016/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COLOMBO-BSCCOMP-PT-202-016
+//
+//  Created by Sasri on 2022-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
