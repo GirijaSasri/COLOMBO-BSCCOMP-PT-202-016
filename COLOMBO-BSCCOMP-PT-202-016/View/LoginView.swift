2d150556915a7c623753a1f09ff9d231b7a8a5d0
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-03 15:10:21 -0700

    Added Login & Registration View
    -Registration View
    -Login View

--- COLOMBO-BSCCOMP-PT-202-016/View/LoginView.swift
+++ COLOMBO-BSCCOMP-PT-202-016/View/LoginView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username=""
+    @State var password=""
+   
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            
+            VStack {
+                Group{
+                    Text("Login")
+                        .font(.system( size: 32, weight: .bold))
+                        .padding(.bottom)
+                    
+                    Text("login with your email and password")
+                }.padding(.top, 100)
+                    
+            TextField("Username", text: $username)
+                .padding(.all)
+           
+            TextField("Password", text: $password)
+                .padding(.all)
+      
+            Button(action:{
+                
+            }) {
+                Text("Login")
+            }
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .clipShape(Capsule())
+                
+                Spacer()
+                
+                
+            
+            
+            
+        }}
     }
 }
 
--- COLOMBO-BSCCOMP-PT-202-016/View/RegisterView.swift
+++ COLOMBO-BSCCOMP-PT-202-016/View/RegisterView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var nic=""
+    @State var dob=""
+    @State var gender=""
+    @State var name=""
+    @State var mobile=""
+    @State var email=""
+    @State var password=""
+    @State var retype_password=""
+    @State var current_location=""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+                Text("Register")
+                .font(.system( size: 32, weight: .bold))
+                .padding(.bottom)
+                Text("Register with the system")
+            
+                TextField("NIC Number", text: $nic)
+                .padding(.all)
+                TextField("Date Of Birth", text: $dob)
+                .padding(.all)
+                TextField("Gender", text: $gender)
+                .padding(.all)
+                TextField("Name", text: $name)
+                .padding(.all)
+                
+        Group{
+               TextField("Mobile Number", text: $mobile)
+                .padding(.all)
+                TextField("Email", text: $email)
+                .padding(.all)
+                TextField("Password", text: $password)
+                .padding(.all)
+                TextField("Retype Password", text: $retype_password)
+                .padding(.all)
+                TextField("Current Location", text: $current_location)
+                .padding(.all)
+            }
+                
+            Button(action:{
+                
+            }) {
+                Text("Sign Up")
+            }
+            
+             
+                 
+                }
+        
     }
 }
 
