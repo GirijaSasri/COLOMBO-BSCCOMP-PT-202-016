199ab69a150cbe2f20413089bb18ec8c968d9b53
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-03 11:37:49 -0700

    Adding Launch Screen
    -Adding the launch screen Design.
    -Adding 1x,2x,3x images for Nibm logo.

---
+++ COLOMBO-BSCCOMP-PT-202-016/View/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  COLOMBO-BSCCOMP-PT-202-016
+//
+//  Created by Sasri on 2022-04-03.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
