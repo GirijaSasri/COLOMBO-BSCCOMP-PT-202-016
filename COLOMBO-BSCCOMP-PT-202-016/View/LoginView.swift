13edcef0f633f4bd49c6f64bfbdab1514b7a71d7
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-04 09:33:40 -0700

    Aligning the login interface
    -image Align
    -text field Align

--- COLOMBO-BSCCOMP-PT-202-016/View/LoginView.swift
+++ COLOMBO-BSCCOMP-PT-202-016/View/LoginView.swift
@@ -16,77 +16,80 @@ struct LoginView: View {
         NavigationView{
    
             VStack {
-           
-                
-                Group{
+            
+               VStack{
                     Text("Login")
                         .font(.system( size: 32, weight: .bold))
                         .padding(.bottom)
                         .foregroundColor(.white)
                     
-                    Text("login with your email and password")
-                }.padding(.top, 100)
-                .foregroundColor(.white)
+                    Text("Login With Your Email And Password")
+                  Image("LoginLogo")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width:250)
+                   
+                    }
+                       .padding(.top)
+                       .padding(.bottom)
+                       .foregroundColor(.white)
                 
-                
-                  
-          HStack{
-          
+             HStack{
+              
                 TextField("Username", text: $username)
-//                .padding(.all)
-                  .font(.title2)
-                  .foregroundColor(.white)
-                 
+//                  .padding(.all)
+                    .font(.title2)
+                    .foregroundColor(.white)
                   
+               
+                  }
+                    .padding()
+                    .background(Color.white.opacity(0.12))
+                    .cornerRadius(15)
+                    .padding(.horizontal)
                 
-                }
-          .padding()
-          .background(Color.white.opacity(0.12))
-          .cornerRadius(15)
-          .padding(.horizontal)
-                
-                HStack{
+            HStack{
            
                 TextField("Password", text: $password)
-//                .padding(.all)
-                        .font(.title2)
-                        .foregroundColor(.white)
+//                  .padding(.all)
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    
+                
                 }
-                .padding()
-                .background(Color.white.opacity(0.12))
-                .cornerRadius(15)
-                .padding(.horizontal)
+                    .padding()
+                    .background(Color.white.opacity(0.12))
+                    .cornerRadius(15)
+                    .padding(.horizontal)
          
            
             
             
-            Button(action:{
-                
-            }) {
-            
-                Text("Login")
+            Button(action:{})
+               {Text("Login")
                     .fontWeight(.heavy)
                     .foregroundColor(.black)
                     .padding(.vertical)
                     .frame(width:UIScreen.main.bounds.width - 150)
                     .background(Color("button"))
                     .clipShape(Capsule())
-                
-            }
+               }
         
 //            .padding()
 //            .background(Color(red: 0, green: 0, blue: 0.5))
 //            .clipShape(Capsule())
                 
+               
+                
+                
+            
                 Spacer()
-                
-                
             
-            
-            
-        }
+              }
             .background(Color("AppTextColor").ignoresSafeArea(.all,edges: .all))
+           
         }
+        
        
     }
 }
