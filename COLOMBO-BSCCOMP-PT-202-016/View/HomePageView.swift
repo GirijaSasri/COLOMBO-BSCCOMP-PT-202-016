1150c4deb1df798196dcbf0842adf06af6da0eb8
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-07 21:34:19 -0700

    HomepageView designing
    -listing house

--- COLOMBO-BSCCOMP-PT-202-016/View/HomePageView.swift
+++ COLOMBO-BSCCOMP-PT-202-016/View/HomePageView.swift
@@ -12,22 +12,66 @@ struct HomePageView: View {
   
     
     var body: some View {
-        ZStack{
-        Color("HomePageBackgroud")
-            .edgesIgnoringSafeArea(.all)
-            
-            VStack (alignment:.leading){
-                AppBarView()
+        
+        
+        ScrollView {
+            ZStack{
+            Color("HomePageBackgroud")
+                .edgesIgnoringSafeArea(.all)
                 
-                TagLingView()
-                    .padding()
-                
-                LandSeacrhView()
-              
+                VStack (alignment:.leading){
+                    AppBarView()
+                    
+                    TagLingView()
+                        .padding()
+                    
+                    LandSeacrhView()
+                        .padding()
+                       
+                    VStack{
+                    Text("Top Ads")
+                        .fontWeight(.bold)
+                        .font(.title2)
+                        .foregroundColor(Color("BlackColor"))
+                        .padding(.horizontal)
+                    
+                    ScrollView (.horizontal,showsIndicators: false){
+                        HStack {
+                            ForEach(0 ..< 4) { index in
+                                ItemCardView(image:Image("DemoHouseImage_\(index + 1)"), size: 210)
+                            }
+                            .padding(.trailing)
+                        }
+                        .padding(.leading)
+                    }
+                    
+                      Text("Trending Ads")
+                          .fontWeight(.bold)
+                          .font(.title2)
+                          .foregroundColor(Color("BlackColor"))
+                          .padding(.horizontal)
+                          .padding(.leading)
+                      
+                      ScrollView (.horizontal,showsIndicators: false){
+                          HStack {
+                              ForEach(0 ..< 4) { index in
+                                  ItemCardView(image:Image("DemoHouseImage_\(index + 1)"), size: 180)
+                              }
+                              .padding(.trailing)
+                          }
+                          .padding(.leading)
+                      }
+                    }
+                    
+                    
+                }
                 
             }
-            
         }
+        
+        // will display the naigation bar
+        
+        
     }
 }
 
@@ -95,5 +139,39 @@ struct LandSeacrhView: View {
             
         }
         .padding(.horizontal)
+    }
+}
+
+struct ItemCardView: View {
+    let image: Image
+    let size:CGFloat
+    var body: some View {
+        VStack{
+             image
+                .resizable()
+                .frame(width: size, height: 200 * (size/210))
+                .cornerRadius(20.0)
+            
+            Text("Marada House")
+                .font(.title3)
+                .fontWeight(.bold)
+            
+            HStack(spacing: 2){
+                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                    Image("StarIcon")
+                }
+                Spacer()
+                
+                Text("RS50000")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+            }
+            
+        }
+        .frame(width:size)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20.0)
     }
 }
