127ad61cb606dbdc7442f6f6f214484332ecb558
GirijaSasri <69510558+GirijaSasri@users.noreply.github.com>
2022-04-14 17:08:28 -0700

    mock update

--- COLOMBO-BSCCOMP-PT-202-016Tests/MockLoginService.swift
+++ COLOMBO-BSCCOMP-PT-202-016Tests/MockLoginService.swift
@@ -5,17 +5,17 @@
 //  Created by Sasri on 2022-04-14.
 //
 
-import Foundation
-
-@testable import COLOMBO_BSCCOMP_PT_202_016
-
-class MockLoginService: LoginServiceProtocol{
-
-    var loginResult: Result<Void, Error> = .success(())
-
-    func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(loginResult)
-    }
-
-}
+//import Foundation
+//
+//@testable import COLOMBO_BSCCOMP_PT_202_016
+//
+//class MockLoginService: LoginServiceProtocol{
+//
+//    var loginResult: Result<Void, Error> = .success(())
+//
+//    func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+//        completion(loginResult)
+//    }
+//
+//}
 
